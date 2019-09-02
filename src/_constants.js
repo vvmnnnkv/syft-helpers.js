e6bf54c7478023fcd9e5d252fa5b49a094007bc4
cereallarceny <me@patrickcason.com>
2019-09-02 17:57:40 +0100

    Removing constant

--- src/_constants.js
+++ src/_constants.js
@@ -7,7 +7,6 @@ export const GET_PLANS = 'get-plans';
 
 // WebRTC
 export const WEBRTC_JOIN_ROOM = 'webrtc: join-room';
-export const WEBRTC_NEW_PEER = 'webrtc: new-peer';
 export const WEBRTC_INTERNAL_MESSAGE = 'webrtc: internal-message';
 export const WEBRTC_PEER_LEFT = 'webrtc: peer-left';
 
